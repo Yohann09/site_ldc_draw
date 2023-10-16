fb17c2f5c105ac94c6414f76f34acee3c444fe44
Flavien Audibert <flavien.audibert@eleves.enpc.fr>
2023-10-16 12:15:12 +0200

    j'espere ca marche

--- LDC.py
+++ LDC.py
@@ -3,6 +3,8 @@ import numpy as np
 import copy
 
 
+# si ça s'affiche c est que j'ai réussi à git push# si ça s'affiche
+# c est que j'ai réussi à git push
 class team:
     def __init__(self, name, country, group, rank, drawn):
         self._name = name
