0d734c4364b539022da7168b7d9510c7f8f1228c
Yohann Canavese <yohann.canavese@eleves.enpc.fr>
2023-10-30 11:01:07 +0100

    petit test

--- UCL_flask/static/js/main.js
+++ UCL_flask/static/js/main.js
@@ -1,3 +1,14 @@
+// Test du chargement des probas:
+
+const resultat = fetch("/static/resultat7.json")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // Les données JSON sont disponibles ici
+  })
+  .catch(error => {
+    console.error('Erreur de chargement du fichier JSON :', error);
+  });
+
 document.addEventListener("DOMContentLoaded", function() {
     // Sélectionnez l'élément h1 par son ID
     let monTitre = document.getElementById("mon-titre");
@@ -9,8 +20,8 @@ document.addEventListener("DOMContentLoaded", function() {
 let boutonContainer = document.getElementById("bouton-container");
 
 // Créez un tableau de noms de boutons
-let Winners = ["Napoli", "FC Porto", "FC Bayern", "Tottenham", "Chelsea FC", "Real Madrid", "Man. City", "Benfica"];
-let Runners_up = ["Liverpool", "FC	Brugge", "Inter", "Frankfurt", "AC Milan", "Leipzig", "Dortmund", "Paris SG"]
+let Winners = ["Napoli", "FC_Porto", "FC_Bayern", "Tottenham", "Chelsea_FC", "Real_Madrid", "Man_City", "Benfica"];
+let Runners_up = ["Liverpool", "FC_Brugge", "Inter", "Frankfurt", "AC_Milan", "Leipzig", "Dortmund", "Paris_SG"]
 
 let affichage_winners = true  // pour savoir quel type de boutons est affiché
 
@@ -94,6 +105,12 @@ function add_team_to_list_match(bouton){
         button.style.display="block"
     })
     chosen_team.push(bouton)    // Rajoute l'équipe dans les équipes choisies
+    // Illumine la colonne en jaune
+    let selecteur = "."+bouton.textContent
+    let colorChange = document.querySelectorAll(selecteur)
+    colorChange.forEach(function (element){
+        element.style.backgroundColor = "#fdfd68"
+    })
     // Ajoute la liste des matchs en fonction des clics de l'utilisateur
     let number = chosen_team.length
     let i = 1+(-1)**(number%2)
@@ -111,7 +128,7 @@ for(let i=0; i<boutons_winners.length; i++) {
             affichage_winners = false
         }
     });
-} // Même fonction pour cette boucle
+} // Même utilité pour cette boucle
 for(let i=0; i<boutons_runner.length; i++) {
     boutons_runner[i].addEventListener("click", function (event) {
         if(!affichage_winners) {
@@ -128,6 +145,29 @@ let undo_button = document.getElementById("undo")
 undo_button.addEventListener("click", function(event){
     if(chosen_team.length !== 0){
         let last_team_chosen = chosen_team.pop()
+        let selecteur = "."+last_team_chosen.textContent
+        let colorChange = document.querySelectorAll(selecteur)
+
+        colorChange.forEach(function (element){
+            let chosen_team_text = []
+            chosen_team.forEach(function(button){
+                chosen_team_text.push(button.textContent)
+            })
+            console.log(element.classList)
+            if(element.classList.length === 3) {
+                let team_test = "defaut"
+                if (element.classList[1] === last_team_chosen.textContent) {
+                    team_test = element.classList[2]
+                    console.log(team_test)
+                } else {
+                    team_test = element.classList[1]
+                    console.log(team_test)
+                }
+                if (!(chosen_team_text.includes(team_test))) {
+                    element.style.backgroundColor = "transparent"
+                }
+            }else if(element.classList.length===2){element.style.backgroundColor = "transparent"}
+        })
         //last_team_chosen.style.display = "block"    // on affiche de nouveau l'équipe
         if(affichage_winners){
             boutons_runner.push(last_team_chosen)   // on la remet dans la liste des boutons affichés correspondante
@@ -168,7 +208,7 @@ team_line.appendChild(vide)
 Winners.forEach(function(name){
     let team = document.createElement("th")
     team.textContent = name
-    team.className = "team-cell"
+    team.className = "team-cell " + name  // ajoute une classe pour que la cellule s'illumine quand équipe sélectionnée
     team_line.appendChild(team)
 })
 team_line.className = "team-line"
@@ -180,12 +220,12 @@ for(let i=0; i<Runners_up.length; i++){
         if(j===0) {
             let team = document.createElement("td")
             team.textContent = Runners_up[i]
-            team.className = "cell-team"
+            team.className = "cell-team " + Runners_up[i] // ajoute une classe pour que la cellule s'illumine quand équipe sélectionnée
             line.appendChild(team)
         }else{
             let cell = document.createElement("td")
             cell.textContent = String(i)+String(j)
-            cell.className = "proba-cell"
+            cell.className = "proba-cell " + Winners[j-1] +" "+ Runners_up[i] // ajoute une classe pour que la cellule s'illumine quand équipe sélectionnée
             line.appendChild(cell)
         }
     }
